78199589f37c2598c17ca23a3feaf1ff3bb662e5
github username <github email address>
2020-09-15 15:22:01 -0700

    got in the right direction, still trying to fix scroll view

--- components/Card.js
+++ components/Card.js
@@ -4,13 +4,13 @@ import { StyleSheet, Text, View } from "react-native";
 export default function home(props) {
   return (
     <View style={styles.card}>
-      <Text style={styles.text}>{props.cat.name}</Text>
+      <Text style={styles.text}>{props.cat}</Text>
     </View>
   );
 }
 const styles = StyleSheet.create({
   card: {
-    width: "90%",
+    width: "80%",
     height: "20%",
     justifyContent: "center",
     backgroundColor: "yellow",
--- components/home.js
+++ components/home.js
@@ -3,26 +3,27 @@ import {
   StyleSheet,
   TouchableOpacity,
   SafeAreaView,
-  ScrollView
+  ScrollView,
+  Dimensions,
+  View
 } from "react-native";
 import Card from "./Card.js";
 
 export default function home(props) {
+  const windowHeight = Dimensions.get("window").height - 50;
   return (
-    <SafeAreaView style={styles.box}>
+    <SafeAreaView style={{ flex: 1, maxHeight: windowHeight }}>
       <ScrollView style={styles.view}>
-        {props.cat.map(categ => (
-          <Card cat={categ} key={categ.name} />
-        ))}
+        {props.cat.map(categ => {
+          return <Card cat={categ.name} key={categ.id} />;
+        })}
       </ScrollView>
     </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
   view: {
+    flex: 1,
     backgroundColor: "pink"
-  },
-  box: {
-    flex: 1
   }
 });
