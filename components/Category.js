317178f3ea053f783a2bbbae1f06c5cf9eeabeef
github username <github email address>
2020-09-17 12:26:34 -0700

    minor bug fixes to join groups

--- components/Category.js
+++ components/Category.js
@@ -3,35 +3,36 @@ import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
 
 export default function Category(props) {
   const join = () => {
-      if(usercat.includes(props.cat.id)){
-        
-      } else {
-    fetch("http://127.0.0.1:3000/user_categories", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        usercat: {
-          user_id: props.signed.user.id,
-          category_id: props.cat.id
-        }
+    if (props.usercat.includes(props.cat.id)) {
+    } else {
+      fetch("http://127.0.0.1:3000/user_categories", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          usercat: {
+            user_id: props.signed.user.id,
+            category_id: props.cat.id
+          }
+        })
       })
-    })
-      .then(res => res.json())
-      .then(res => console.log(res))
-    let placeholder = props.usercat
-    placeholder.push(props.cat.id)
-    setUsercat(placeholder)
-}
+        .then(res => res.json())
+        .then(res => console.log(res));
+      let placeholder = props.usercat;
+      placeholder.push(props.cat.id);
+      setUsercat(placeholder);
+    }
   };
   return (
     <View style={styles.view}>
       <Text style={styles.title}>{props.cat.name}</Text>
       <Text style={styles.desc}>{props.cat.desc}</Text>
       <TouchableOpacity onPress={() => join()} style={styles.join}>
-        <Text>{(usercat.includes(props.cat.id))? "Leave Group" : "Join Group"</Text>
+        <Text>
+          {props.usercat.includes(props.cat.id) ? "Leave Group" : "Join Group"}
+        </Text>
       </TouchableOpacity>
       <TouchableOpacity
         onPress={() => props.changeDisplay(0)}
